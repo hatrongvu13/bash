5c82d083dc73a4d46b45784c6119a8b04b7c7892
Hà Trọng Vũ <46570997+hatrongvu13@users.noreply.github.com>
2021-07-19 22:52:52 +0700

    bash reset idea win

--- win/idea.cmd
+++ win/idea.cmd
@@ -9,3 +9,15 @@ for %%I in ("WebStorm", "IntelliJ", "CLion", "Rider", "GoLand", "PhpStorm", "Res
 REM Delete registry key and jetbrains folder (not sure if needet but however)
 rmdir /s /q "%APPDATA%\JetBrains"
 reg delete "HKEY_CURRENT_USER\Software\JavaSoft" /f
+
+======
+@echo off
+REM Delete eval folder with licence key and options.xml which contains a reference to it
+for %%I in ("WebStorm", "IntelliJ", "CLion", "Rider", "GoLand", "PhpStorm", "Resharper", "PyCharm") do (
+    for /d %%a in ("%APPDATA%\JetBrains\%%I*") do (
+        rd /s /q "%%a/eval"
+        del /q "%%a\options\other.xml"
+    )
+)
+
+reg delete "HKEY_CURRENT_USER\SOFTWARE\JavaSoft\Prefs\jetbrains" /f
