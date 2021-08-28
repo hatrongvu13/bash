5b955ca157ccde54e19656600a9a40a860fff181
hatrongvu13 <hatrongvu13@gmail.com>
2021-08-29 05:48:57 +0700

    bash color

--- linux/colorList.sh
+++ linux/colorList.sh
@@ -1,22 +1,28 @@
-# for x in {0..2}; do
-#     for i in {30..37}; do
-#         for a in {40..47}; do
-#             echo -ne "\e[$x;$i;$a""m\\\e[$x;$i;$a""m\e[0;37;40m"
-#         done
-#         echo
-#     done
-# done
-# echo ""
-
+for x in {0..8}; do
+    for i in {30..37}; do
+        for a in {40..47}; do
+            echo -ne "\e[$x;$i;$a""m\\\e[$x;$i;$a""m\e[0;37;40m"
+        done
+        echo
+    done
+done
+echo ""
+############################
+for i in {30..37}; do
+    for a in {40..47}; do
+        echo -ne "\e[$x;$i;$a""m\\\e[$x;$i;$a""m\e[0;37;40m"
+    done
+    echo
+done
 ############################
 
 # for i in {30..37}; do
-#     echo -e "\e[03;05;$i""mcolor"
-#     echo -e "\033[01;$i""mas"
-#     # for a in {40..47}; do
-#     #     # echo -ne "\e[$x;$i;$a""m\\\e[$x;$i;$a""m\e[0;37;40m"
-#     #     echo -e "\e[$x;$i;$a""m\\\e[$x;$i;$a""m\e[0;37;40m"
-#     # done
+    # echo -e "\e[03;05;$i""mcolor"
+    # echo -e "\033[01;$i""mas"
+    # for a in {40..47}; do
+    #     # echo -ne "\e[$x;$i;$a""m\\\e[$x;$i;$a""m\e[0;37;40m"
+    #     echo -e "\e[$x;$i;$a""m\\\e[$x;$i;$a""m\e[0;37;40m"
+    # done
 #     echo
 # done
 
@@ -38,21 +44,21 @@
 # done
 ##############################
 
-T='gYw'   # The test text
+# T='gYw'   # The test text
 
-echo -e "\n                 40m     41m     42m     43m\
-     44m     45m     46m     47m";
+# echo -e "\n                 40m     41m     42m     43m\
+#      44m     45m     46m     47m";
 
-for FGs in '    m' '   1m' '  30m' '1;30m' '  31m' '1;31m' '  32m' \
-           '1;32m' '  33m' '1;33m' '  34m' '1;34m' '  35m' '1;35m' \
-           '  36m' '1;36m' '  37m' '1;37m';
-  do FG=${FGs// /}
-  echo -en " $FGs \033[$FG  $T  "
-  for BG in 40m 41m 42m 43m 44m 45m 46m 47m;
-    do echo -en "$EINS \033[$FG\033[$BG  $T  \033[0m";
-  done
-  echo;
-done
-echo
+# for FGs in '    m' '   1m' '  30m' '1;30m' '  31m' '1;31m' '  32m' \
+#            '1;32m' '  33m' '1;33m' '  34m' '1;34m' '  35m' '1;35m' \
+#            '  36m' '1;36m' '  37m' '1;37m';
+#   do FG=${FGs// /}
+#   echo -en " $FGs \033[$FG  $T  "
+#   for BG in 40m 41m 42m 43m 44m 45m 46m 47m;
+#     do echo -ne "$EINS \033[$FG\033[$BG  "\033[$FG\033[$BG"  \033[0m";
+#   done
+#   echo;
+# done
+# echo
 
 ##############################
