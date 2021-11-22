b48387dd2128919f31b83a8efebf5069152017ed
hatrongvu13 <hatrongvu13@gmail.com>
2021-11-22 14:00:26 +0700

    update code reset idea

--- linux/resetJetbrains.sh
+++ linux/resetJetbrains.sh
@@ -1,14 +1,16 @@
-#!/bin/bash
-#!/bin/sh
-# reset jetbrains ide evals
-
-OS_NAME=$(uname -s)
-JB_PRODUCTS="IntelliJIdea CLion PhpStorm GoLand PyCharm WebStorm Rider DataGrip RubyMine AppCode"
-
-for PRD in $JB_PRODUCTS; do
-	rm -rf ~/.java/.userPrefs/prefs.xml
-	rm -rf ~/.java/.userPrefs/jetbrains/prefs.xml
-	rm -rf ~/.config/JetBrains/${PRD}*/eval/
-	rm -rf ~/.config/JetBrains/${PRD}*/options/other.xml
-done
-echo "done !"
+##!/bin/bash
+##!/bin/sh
+## reset jetbrains ide evals
+#
+#OS_NAME=$(uname -s)
+#JB_PRODUCTS="IntelliJIdea CLion PhpStorm GoLand PyCharm WebStorm Rider DataGrip RubyMine AppCode"
+#
+#for PRD in $JB_PRODUCTS; do
+#	rm -rf ~/.java/.userPrefs/prefs.xml
+#	rm -rf ~/.java/.userPrefs/jetbrains/prefs.xml
+#	rm -rf ~/.config/JetBrains/${PRD}*/eval/
+#	rm -rf ~/.config/JetBrains/${PRD}*/options/other.xml
+#done
+#echo "done !"
+rm -rf ~/.java/.userPrefs
+rm -rf ~/.config/JetBrains
